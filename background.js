de1ba44b1c6a10ccad3cc31296f5273492646a4e
Timothy Soehnlin <tim@eaiti.com>
2014-02-05 18:58:17 -0600

    Initial import for filter

--- background.js
+++ background.js
@@ -1,9 +1,4 @@
 window.onload = function() {
-  // Set default values
-  if (!localStorage.wordList) {
-    localStorage.wordList = "asshole,bitch,cock,cunt,damn,fuck,piss,slut,shit,tits,whore";
-  }
-
   // Handle Message Passing for localStorage
   chrome.extension.onConnect.addListener(function(port) {
     port.onMessage.addListener(function(msg) {
